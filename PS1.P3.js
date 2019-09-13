323e05fda62e938d13786f4a5e9c2a0e319fcf25
Normandie <nessig@bu.edu>
2019-09-13 12:04:37 -0400

    all done

--- PS1.P3.js
+++ PS1.P3.js
@@ -1,25 +1,41 @@
 //function that accepts two input parameters: a string, and a function. The function
 //should execute the passed function with the passed string and return the result.
-
-
 //pass the string ‘supercalifragilisticexpialidocious’ and a lambda function that returns an array containing
 // fragments of the input string broken on the character ‘c’
 
-const which = string => split_string(string);
 
-const split_string = string => string.split('c');
-
-
-const replace = string => {
-    console.log(`originalString: ${string}`);
-    console.log(`modifiedString: ${string.replace(/a/, 'A')}`);
-    console.log(`numberReplaced: ${5}`);
-    console.log(`length: ${string.length}`);
-    return
+const find_num_dif = (string,mString) => {
+    let result = 0 ;
+    for (i = 0; i < string.length ; i++){
+        if (string[i] !== mString[i])
+            result ++;
+    }
+    return result
 }
 
+const info_object = (string, mString) => {
 
-console.log(`${which('supercalifragilisticexpialidocious', replace)}`)
+    numdif = find_num_dif(string, mString);
+
+    var info =  {
+        originalString: string,
+        modifiedSting: mString,
+        numberReplaced: numdif,
+        length: string.length
+    }
+    return info;
+}
+
+const m_function = (string, lambda_function) => lambda_function(string);
 
 
-replace('supercalifragilisticexpialidocious');
+console.log(`Expression 1: ${m_function("supercalifragilisticexpialidocious", string => string.split(/(?=c)/g))}`);
+
+var results = info_object("supercalifragilisticexpialidocious", (m_function("supercalifragilisticexpialidocious", string => string.replace(/a/g,'A'))));
+
+console.log("Expression 2:");
+console.log(results);
+
+
+
+
