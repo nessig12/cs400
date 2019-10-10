60696ea8c5f43e7a4ddb1281c5c2c5e1871ebf82
Normandie <nessig@bu.edu>
2019-10-10 14:16:16 -0400

    deleted old files

--- PS2.P2.js
+++
@@ -1,7 +0,0 @@
-// Write a generator that is initialized with a sentence and that emits each word of the sentence in
-// turn.
-//     Use the generator to print the words, one per line, of the string “All I know is something like a
-// bird within her sang”.
-
-const one_word = words => words.replace(/ /g, "\n");
-console.log(`One word at a time:\n${one_word("All I know is something like a bird within her sang")}`);
