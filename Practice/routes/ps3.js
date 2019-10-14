cdcccbcdd2b5c22c30073d7ced6a89798453ea14
Normandie <nessig@bu.edu>
2019-10-14 18:14:00 -0400

    post finished

--- Practice/routes/ps3.js
+++ Practice/routes/ps3.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const request = require("request");
+const request = require('request');
 
-router.get('/',function (req, res, next) {
-    const options = {
-        method: 'GET',
-        url: ' http://www.omdbapi.com/?i=tt3896198&apikey=ae9419f7', //'http://www.omdbapi.com/?t=mean+girls',
-    };
-    request(options, function(error, response, body) {
-        console.log(`Response: ${response}`);
-        if (error) throw new Error(error);
+router.route('/')
+    .get(function (req, res, next) {
+        const options = {method: 'GET', url: ' http://www.omdbapi.com/?i=tt3896198&apikey=ae9419f7',};
+        request(options, function (error, response, body) {
+            console.log(`Response: ${response}`);
+            if (error) throw new Error(error);
 
-        console.log(`Body: ${body}`);
+            console.log(`Body: ${body}`);
 
-        const result = JSON.parse(body);
+            const result = JSON.parse(body);
 
-        res.render('ps3',
-            {
-                title: "CS 400 Assignment",
-                name: result.Title,
-                release: result.Released,
-            }
-        )
-    });
-});
+            res.render('ps3',
+                {
+                    name: result.Title,
+                    release: result.Released,
+                }
+            )
+        })
+    })
+    .post(function (req, res, next) {
+        let stuff = req.body;
 
-router.post('/',function (req, res, next) {
-    let stuff = req.body;
-    res.render('ps3', stuff );
-});
+        console.log(stuff);
+        res.render('ps3', stuff);
+    })
+
+
 
 module.exports = router;
