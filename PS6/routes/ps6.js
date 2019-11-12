24e13feb89aeb398548c08dc7ee04f88c3e63ee8
Normandie <nessig@bu.edu>
2019-11-12 17:20:53 -0500

    im working

--- PS6/routes/ps6.js
+++ PS6/routes/ps6.js
@@ -14,8 +14,7 @@ db.connect((err,client) => {
 
 router.route('/')
     .post(function (req, res, next) {
-        //console.log("im after the form submitted");
-        const character_n = 'Ron Weasley'; //req.params.name;
+        const character_n = req.body.character;
 
         //which character did the person choose?
         let id;
@@ -28,80 +27,83 @@ router.route('/')
         else if (character_n === ('Draco Malfoy')){
             id = '5a109f8b3dc2080021cd8795';
         }
+        else if (character_n === ('Luna Lovegood')){
+            id = '5a109e943dc2080021cd8791';
+        }
 
         //connect to db
         let mongo = db.getDB();
 
         //check if term is in database !!! either ronweasely or harrypotter
-        let in_database = (mongo.collection('hpcharacters').findOne({name: character_n}) != null); ///ask perry about this line cuz its always true
 
-        mongo.collection('hpcharacters').find({}).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
+        let in_database;
+
+        mongo.collection('hpcharacters').findOne({name: character_n}, (err, user) => {
+            if (err) {
+                // handle error
+                console.log(`Error occured`);
+            }
+            if (user) {
+                //console.log(`user exist`);
+                in_database = true;
+                //console.log(`in database? ::: ${in_database}`);
+                let hp_house = (mongo.collection('hpcharacters').findOne({name: character_n}, {house:1}));
+
+                //console.log(`Result from database ${hp_house}`);
+
+                hp_house.then(function(result) {
+                    //console.log(result); // "Some User token"
+
+                    res.render('index',
+                        {
+                            title: "CS400 Assignment 4 ",
+                            character_n: character_n,
+                            house: result.house,
+                            cache: in_database,
+                        }
+                    )
+                })
+            } else {
+                // there is no user
+                //console.log(`user DOES NOT exist`);
+                in_database = false;
+                //console.log(`in database? ::: ${in_database}`);
+
+                const options = {
+                    method: 'GET',
+                    url: 'https://www.potterapi.com/v1/characters/' + id,
+                    qs: {
+                        key: '$2a$10$8iXOkF8dl5qOG57eGMO5FO9EDcMeU1Xi7G3ybFVipqnhzUD9Xs0v6',
+                    }
+                };
+                request(options, function (error, response, body) {
+                    console.log(`Response: ${response}`);
+                    if (error) throw new Error(error);
+
+                    console.log(`Body: ${body}`);
+
+                    const result = JSON.parse(body);
+
+                    console.log(`result: ${result}`);
+
+                    mongo.collection('hpcharacters').insertOne({name:character_n, house:result.house}, function (err,r){
+                        //res.send('success');
+                    });
+
+                    res.render('index',
+                        {
+                            title: "CS400 Assignment 4 ",
+                            character_n: character_n,
+                            house: result.house,
+                            cache: in_database,
+                        }
+                    )
+                })
+            }
         });
-
-        if (in_database){
-            let hp_house = (mongo.collection('hpcharacters').findOne({name: character_n}, {house:1}));
-
-            console.log(`Result from database ${hp_house}`);
-
-            hp_house.then(function(result) {
-                console.log(result); // "Some User token"
-
-                res.render('index',
-                    {
-                        title: "CS400 Assignment 4 ",
-                        character_n: character_n,
-                        house: result.house,
-                        cache: in_database,
-                    }
-                )
-            })
-
-
-        }
-        else { // info is not cached get it from API!!!
-            const options = {
-                method: 'GET',
-                url: 'https://www.potterapi.com/v1/characters/' + id,
-                qs: {
-                    key: '$2a$10$8iXOkF8dl5qOG57eGMO5FO9EDcMeU1Xi7G3ybFVipqnhzUD9Xs0v6',
-                }
-            };
-            request(options, function (error, response, body) {
-                console.log(`Response: ${response}`);
-                if (error) throw new Error(error);
-
-                console.log(`Body: ${body}`);
-
-                const result = JSON.parse(body);
-
-                console.log(`result: ${result}`);
-
-                mongo.collection('hpcharacters').insertOne({name:character_n, house:result.house}, function (err,r){
-                    //res.send('success');
-                });
-
-                res.render('index',
-                    {
-                        title: "CS400 Assignment 4 ",
-                        character_n: character_n,
-                        house: result.house,
-                        cache: in_database,
-                    }
-                )
-            })
-        }
     });
 
 router.route('/')
-    .get(function (req, res, next) {
-        console.log("im before the form submitted");
-        res.render('form',
-            {
-                title: "CS400 Assignment 4 ",
-            }
-        )
-    });
+    .get(function (req, res, next) {res.render('form', {title: "CS400 Assignment 4 ",})});
 
 module.exports = router;
