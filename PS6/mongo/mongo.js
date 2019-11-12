6dc6bbfb283d3b2f4e258ac5fcc3149b546db7b1
Normandie <nessig@bu.edu>
2019-11-12 11:59:25 -0500

    im so close

--- PS6/mongo/mongo.js
+++ PS6/mongo/mongo.js
@@ -7,7 +7,7 @@ module.exports = {
 
     connect: function( callback ) {
         mongoClient.connect( mongoURL,  { useNewUrlParser: true }, function( err, client ) {
-            _db  = client.db('test_db');
+            _db  = client.db('cs400');
             return callback( err );
         } );
     },
--- PS6/routes/ps6.js
+++ PS6/routes/ps6.js
@@ -15,66 +15,83 @@ db.connect((err,client) => {
 router.route('/')
     .post(function (req, res, next) {
         //console.log("im after the form submitted");
-        const character_n = 'HarryPotter'; //req.params.name;
+        const character_n = 'Ron Weasley'; //req.params.name;
 
         //which character did the person choose?
-        // -- fix this to be get the result from the form, to get only that response from the api
-        //rather than all of the characters and index
-        let num;
-        if (character_n == 'HarryPotter'){
-            num = 109;
+        let id;
+        if (character_n === ('Harry Potter')){
+            id = '5a12292a0f5ae10021650d7e';
         }
-        else {// ron weasely
-            num = 150;
+        else if (character_n === ('Ron Weasley')){// ron weasely
+            id = '5a1239c80f5ae10021650dad';
         }
-
-
-        //Database cache or call API???
+        else if (character_n === ('Draco Malfoy')){
+            id = '5a109f8b3dc2080021cd8795';
+        }
 
         //connect to db
         let mongo = db.getDB();
 
-        let in_database = false;
         //check if term is in database !!! either ronweasely or harrypotter
+        let in_database = (mongo.collection('hpcharacters').findOne({name: character_n}) != null); ///ask perry about this line cuz its always true
+
+        mongo.collection('hpcharacters').find({}).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
 
         if (in_database){
-            const options = {
-               // mongo.connection('hpcharacters').find({name:req.params.name})
-            }
+            let hp_house = (mongo.collection('hpcharacters').findOne({name: character_n}, {house:1}));
+
+            console.log(`Result from database ${hp_house}`);
+
+            hp_house.then(function(result) {
+                console.log(result); // "Some User token"
+
+                res.render('index',
+                    {
+                        title: "CS400 Assignment 4 ",
+                        character_n: character_n,
+                        house: result.house,
+                        cache: in_database,
+                    }
+                )
+            })
+
 
         }
         else { // info is not cached get it from API!!!
             const options = {
                 method: 'GET',
-                url: 'https://www.potterapi.com/v1/characters',
+                url: 'https://www.potterapi.com/v1/characters/' + id,
                 qs: {
                     key: '$2a$10$8iXOkF8dl5qOG57eGMO5FO9EDcMeU1Xi7G3ybFVipqnhzUD9Xs0v6',
                 }
             };
-            mongo.collection('hpcharacters').insertOne("HarryPotter", function (err,r){
-                res.send('success');
-            });
+            request(options, function (error, response, body) {
+                console.log(`Response: ${response}`);
+                if (error) throw new Error(error);
+
+                console.log(`Body: ${body}`);
+
+                const result = JSON.parse(body);
+
+                console.log(`result: ${result}`);
+
+                mongo.collection('hpcharacters').insertOne({name:character_n, house:result.house}, function (err,r){
+                    //res.send('success');
+                });
+
+                res.render('index',
+                    {
+                        title: "CS400 Assignment 4 ",
+                        character_n: character_n,
+                        house: result.house,
+                        cache: in_database,
+                    }
+                )
+            })
         }
-
-        request(options, function (error, response, body) {
-            console.log(`Response: ${response}`);
-            if (error) throw new Error(error);
-
-            console.log(`Body: ${body}`);
-
-            const result = JSON.parse(body);
-
-            console.log(`result: ${result}`);
-
-            res.render('index',
-                {
-                    title: "CS400 Assignment 4 ",
-                    name: result[num].name,
-                    house: result[num].house,
-                    school: result[num].school,
-                }
-            )
-        })
     });
 
 router.route('/')
@@ -86,4 +103,5 @@ router.route('/')
             }
         )
     });
+
 module.exports = router;
